b428c16feba863b4c4e27667315fe71203443d6c
tiggascode <tigrancode@outlook.com>
2025-04-13 19:45:41 +0400

    Install and Activate DaisyUI

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -5,10 +5,11 @@ import ResponsiveNavLink from '@/Components/Core/ResponsiveNavLink';
 import {Link, usePage} from '@inertiajs/react';
 import {PropsWithChildren, ReactNode, useState} from 'react';
 
-export default function Authenticated({
-                                        header,
-                                        children,
-                                      }: PropsWithChildren<{ header?: ReactNode }>) {
+export default function AuthenticatedLayout(
+  {
+    header,
+    children,
+  }: PropsWithChildren<{ header?: ReactNode }>) {
   const user = usePage().props.auth.user;
 
   const [showingNavigationDropdown, setShowingNavigationDropdown] =
